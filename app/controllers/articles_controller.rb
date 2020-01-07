2177f2faf10467c3bb049901cf1bd4c4d61472f2
flov3rh <floperh@gmail.com>
2020-01-07 15:25:10 -0500

    set up the actions of the Articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,20 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
+    if @article.save?
+      #do nothing
+    else
+      render 'new'
+    end
+  end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title,:content,:user_id)
   end
 end
