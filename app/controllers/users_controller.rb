38ccfb15810f5db14770eff67e23da171e50ecb6
Juan Pablo <jpablomgil@gmail.com>
2020-01-07 15:39:25 -0600

    Logout helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       #go to user
+      log_in @user
       redirect_to root_path
     else
       render 'new'
