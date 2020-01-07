e0a922c1c9f72fab6b36ef32a69d619635b409b6
Juan Pablo <jpablomgil@gmail.com>
2020-01-07 11:51:42 -0600

    Deleting User Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'static_page/home'
   root 'static_page#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  '/signup',  to: 'user#new'
+  post '/signup',  to: 'user#create'
+  resources :user
+
 end
