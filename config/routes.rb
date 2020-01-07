1436bf776a2fd9aeac459d4947fc25e4694d5f6c
Juan Pablo <jpablomgil@gmail.com>
2020-01-07 14:06:03 -0600

    Signup Config and View design

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'static_page/home'
   root 'static_page#home'
-  get  '/signup',  to: 'user#new'
-  post '/signup',  to: 'user#create'
-  resources :user
-
+  get  '/signup',  to: 'users#new'
+  resources :users
 end
