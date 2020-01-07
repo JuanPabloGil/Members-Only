c9db5a36f532f5344153c24889a8e624daac43b1
flov3rh <floperh@gmail.com>
2020-01-07 16:15:15 -0500

    Work on Sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
   def new
+    @session = Session.new
   end
-  
+
+  # def create
+  #   user = User.find_by(email: params[:session][:email].downcase)
+  #   if user && user.authenticate(params[:session][:password])
+  #     # Log the user in and redirect to the user's show page.
+  #   else
+  #     # Create an error message.
+  #     render 'new'
+  #   end
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   root 'static_page#home'
   get  '/signup',  to: 'users#new'
   resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
