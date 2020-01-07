b240240e6d814ce27582fe388ea5ca19d3e667a1
flov3rh <floperh@gmail.com>
2020-01-07 15:39:24 -0500

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'articles/new'
 
   get 'articles/show'
+  get '/publish', to: 'articles#new'
+  resources :articles
 
   root 'static_page#home'
   get  '/signup',  to: 'users#new'
