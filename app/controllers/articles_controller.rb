ff521378a4d9f3b0183747a8066aff57f9103113
Juan Pablo <jpablomgil@gmail.com>
2020-01-08 12:19:15 -0600

    Now  you can publish only be loged in

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,15 @@
 class ArticlesController < ApplicationController
   def new
     @article = Article.new
+    @user = User.new
   end
 
   def create
     @article = Article.new(article_params)
+    @article.user_id = current_user.id if logged_in?
 
     if @article.save
+
       redirect_to root_path
     else
       render 'new'
@@ -22,4 +25,6 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title,:content,:user_id)
   end
+
+
 end
