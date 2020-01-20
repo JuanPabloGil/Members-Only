83f433edcf14407daf6af48df9675e5145da6fbd
flov3rh <floperh@gmail.com>
2020-01-20 14:19:35 -0500

    Create remember_token column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200107165847) do
+ActiveRecord::Schema.define(version: 20200120191819) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20200107165847) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
 end
