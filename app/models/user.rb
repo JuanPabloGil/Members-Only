2841d0b1805821bca5b3f26977e96c2279464709
flov3rh <floperh@gmail.com>
2020-01-20 14:37:44 -0500

    Handle the remember_token in the User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   has_many :articles
+  # before_create remember
   before_save   :downcase_email
   validates :name,  presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -11,10 +13,19 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
   private
 
   def downcase_email
     self.email = email.downcase
   end
-
 end
