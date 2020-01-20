3bb729de3154813cf2fc585281efae2482c2955c
flov3rh <floperh@gmail.com>
2020-01-20 14:53:19 -0500

    Fix bug in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
   has_many :articles
-  # before_create remember
   before_save   :downcase_email
   validates :name,  presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
